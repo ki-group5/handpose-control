6dbcbe06c6a8b61975431fcd14b0a85f8b800a85
Jan Esch <jan.esch@tmnd.de>
2021-06-24 20:42:31 +0200

    update train centroid classifier script

--- classifier/centroid/train_centroid_classifier.py
+++ classifier/centroid/train_centroid_classifier.py
@@ -1,22 +1,18 @@
 import json
-import numpy as np
 import os
 
+from ...load_data import load_data
 
-def train_centroid_classifier(datafile, model_filename='01.json'):
-    data = json.load(datafile)
 
-    shape = data[0].data.shape
-    unique_labels = set(data.labels)
-    centroids = {label: [np.zeros(shape), 0] for label in unique_labels}
+def train_centroid_classifier(model_filename='01.json'):
+    data = load_data()
+    labels = data.keys()
 
-    for d in data:
-        c = centroids[d.label]
-        c[0] += d.data
-        c[1] += 1
-
-    for label, centroid in centroids:
-        centroid = centroid[0] / centroid[1]
+    centroids = {}
+    for label in labels:
+        _data = data[label]
+        num_datapoints = _data.shape[0]
+        centroid = _data.sum(axis=0) / num_datapoints
         centroids[label] = centroid
 
     model_filename = f'{os.path.dirname(os.path.abspath(__file__))}/params/{model_filename}'
