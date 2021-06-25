add small fixes in normalization, add state control and add more code to example main

utils/normalized.py
@@ -8,7 +8,7 @@ by a simple hand pose classifier. The orientation and positional data then can b
 from numpy.linalg import norm
 from utils.converter import convert_mediapipe_to_numpy
 from dataclasses import dataclass
-from typing import Union, Tuple
+from typing import Optional, Union, Tuple
 
 import numpy as np
 from mediapipe.python.solutions.hands import HandLandmark as HL
@@ -25,6 +25,7 @@ class NormalizedData:
     direction: np.ndarray  # nparray: 20x3
     normal: np.ndarray  # Orientation normal (vec3)
     hand: str  # "Left" or "Right" hand
+    rotation: Optional[np.ndarray] = None
 
     @classmethod
     def create_from_mediapipe(cls, landmarks, hand: str) -> "NormalizedData":
@@ -48,28 +49,28 @@ class NormalizedData:
 
         # Calculate direction
         direction = np.array([
-            # Thump
-            dzero[1] - dzero[0],
+            # Thumb
+            dzero[1],
             dzero[2] - dzero[1],
             dzero[3] - dzero[2],
             dzero[4] - dzero[3],
             # Index finger
-            dzero[5] - dzero[0],
+            dzero[5],
             dzero[6] - dzero[5],
             dzero[7] - dzero[6],
             dzero[8] - dzero[7],
             # Middle finger
-            dzero[9] - dzero[0],
+            dzero[9],
             dzero[10] - dzero[9],
             dzero[11] - dzero[10],
             dzero[12] - dzero[11],
             # Ring finger
-            dzero[13] - dzero[0],
+            dzero[13],
             dzero[14] - dzero[13],
             dzero[15] - dzero[14],
             dzero[16] - dzero[15],
-            # Ring finger
-            dzero[17] - dzero[0],
+            # Pinky finger
+            dzero[17],
             dzero[18] - dzero[17],
             dzero[19] - dzero[18],
             dzero[20] - dzero[19],
@@ -78,10 +79,32 @@ class NormalizedData:
         # Normalize the directions to unit vectors
         dlen = np.linalg.norm(direction, axis=1)
         dlen_sel = dlen != 0
-        direction[dlen_sel] = (direction[dlen_sel].T / dlen).T
+        direction[dlen_sel] = (direction[dlen_sel].T / dlen[dlen_sel]).T
 
-        # Rotate the directions so the normal is on the x-axis
+        # # Rotate the directions so the normal is on the x-axis
+        # x = np.array([u, v, normal])
+        # rotation = np.linalg.inv(np.eye(3))
+        # direction = rotation.dot(direction.T).T
+
         quat = Quat.create_from_to(normal, (1, 0, 0))
         direction = quat * direction
 
-        return cls(data, direction, normal, hand)
+        return cls(data, direction, normal, hand, None)
+
+    def reconstruct(self) -> np.ndarray:
+        # d = self.direction * 0.05
+        d = self.direction * 0.05
+
+        data = np.array([
+            (0, 0, 0), d[0], d[0]+d[1], d[0]+d[1]+d[2], d[0]+d[1]+d[2]+d[3],
+            d[4], d[4]+d[5], d[4]+d[5]+d[6], d[4]+d[5]+d[6]+d[7],
+            d[8], d[8]+d[9], d[8]+d[9]+d[10], d[8]+d[9]+d[10]+d[11],
+            d[12], d[12]+d[13], d[12]+d[13]+d[14], d[12]+d[13]+d[14]+d[15],
+            d[16], d[16]+d[17], d[16]+d[17]+d[18], d[16]+d[17]+d[18]+d[19]
+        ])
+        # quat = Quat.create_from_to(self.normal, (1,0,0))
+        # data = quat * data
+
+        data += self.data[0]
+
+        return data

utils/render.py
@@ -2,9 +2,11 @@
 
 from utils.normalized import NormalizedData
 import math
-from typing import Optional, Tuple
+from typing import Dict, List, Optional, Tuple
+import numpy as np
 
 import cv2
+from mediapipe.python.solutions.drawing_utils import DrawingSpec
 
 
 class PoseRender:
@@ -44,3 +46,46 @@ class PoseRender:
 
         if p0 and p1:
             cv2.line(image, p0, p1, (0, 100, 200),  2)
+
+    def render_landmarks(
+            image: np.ndarray,
+            landmark_list: np.ndarray,
+            connections: Optional[List[Tuple[int, int]]] = None,
+            landmark_drawing_spec: DrawingSpec = DrawingSpec(
+                color=(0, 0, 255)),
+            connection_drawing_spec: DrawingSpec = DrawingSpec()):
+        if len(landmark_list) == 0:
+            return
+        if image.shape[2] != 3:
+            raise ValueError(
+                'Input image must contain three channel rgb data.')
+        image_rows, image_cols, _ = image.shape
+        idx_to_coordinates = {}
+        for idx, landmark in enumerate(landmark_list):
+            # if ((landmark.HasField('visibility') and
+            #      landmark.visibility < VISIBILITY_THRESHOLD) or
+            #     (landmark.HasField('presence') and
+            #      landmark.presence < 0.5)):
+            #   continue
+            landmark_px = PoseRender.normalized_to_pixel_coordinates(landmark[0], landmark[1],
+                                                                     image_cols, image_rows)
+            if landmark_px:
+                idx_to_coordinates[idx] = landmark_px
+        if connections:
+            num_landmarks = len(landmark_list)
+            # Draws the connections if the start and end landmarks are both visible.
+            for connection in connections:
+                start_idx = connection[0]
+                end_idx = connection[1]
+                if not (0 <= start_idx < num_landmarks and 0 <= end_idx < num_landmarks):
+                    raise ValueError(f'Landmark index is out of range. Invalid connection '
+                                    f'from landmark #{start_idx} to landmark #{end_idx}.')
+                if start_idx in idx_to_coordinates and end_idx in idx_to_coordinates:
+                    cv2.line(image, idx_to_coordinates[start_idx],
+                            idx_to_coordinates[end_idx], connection_drawing_spec.color,
+                            connection_drawing_spec.thickness)
+        # Draws landmark points after finishing the connection lines, which is
+        # aesthetically better.
+        for landmark_px in idx_to_coordinates.values():
+            cv2.circle(image, landmark_px, landmark_drawing_spec.circle_radius,
+                       landmark_drawing_spec.color, landmark_drawing_spec.thickness)