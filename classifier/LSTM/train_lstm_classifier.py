2da8e7276f19aac9b21087dcd34bd85300c2e65c
Thomas Mathes <thomas.mathes@tmnd.onmicrosoft.com>
2021-06-25 04:36:05 +0200

    send dummy training

--- classifier/LSTM/train_lstm_classifier.py
+++ classifier/LSTM/train_lstm_classifier.py
@@ -21,17 +21,33 @@ from utils.normalized import NormalizedData
 
 
 def train_lstm_classifier(model_filename='01.json'):
-    return
     loader = DataLoader(Path("./data/"))
     data = loader.load_all()
-    unique_labels = set(entry.label for entry in data)
-    centroids = {label: [] for label in unique_labels}
+    #unique_labels = set(entry.label for entry in data)
+    #centroids = {label: [] for label in unique_labels}
+
+    model = Sequential()
+    model.add(LSTM(1,input_shape=(10,21)))
+    model.add(Dense(1))
+
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer='rmsprop',
+                metrics=['accuracy'])
+
+    x_train = np.random.random((100, 10, 21))
+    #print(x_train)
+    y_train = np.random.random((100, 1))
+    print(y_train)
+    model.fit(x_train, y_train, batch_size=300, epochs=20)
+
 
     for entry in data:
         normalized_data = NormalizedData.create(entry.landmarks, entry.hand.name)
         #print(entry.hand.name)
-        #print(entry.label)
-        #print(normalized_data)
+        if(entry.hand.name == "Right"):
+            print(entry.label)
+            #print(normalized_data)
         # TODO: use normalized_data.normal ?
         centroids[entry.label].append(normalized_data.direction)
 
